3427a8b8f9e0992676eadc3d8c5ca5cbe2508c2f
Tyler Trinh <tylervtrinh@gmail.com>
2019-11-22 21:35:12 -0800

    Fix whitespace

--- db_files.py
+++ db_files.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import pandas as pd
+
 """
     The columns (16) contained the in DataFrame are:
     track_id, song_id, title, artist_name, duration, year, artist_mbid, 
@@ -11,11 +12,15 @@ import pandas as pd
     release (album)
     The data was spread across three different databases. The tables of each database
     were combined into one. From there a query was created to JOIN the different tables together.
-    
+
     NOTE: There are some songs that have NULL/irrelevant columns 
     (ex. artist tags (6290), artist terms (5), year of 0 (5320))
     (Using INNER JOIN (3703))
 """
+
+# TODO: 
+# - Similar artist column contains a string of artist ids separated by "|"
+# -- Should we display artist names instead?
 
 def read_data(filename):
     return pd.read_csv(filename)
@@ -23,12 +28,8 @@ def read_data(filename):
 def main(filename):
 
     # Read in the tracks_agg.csv (7MB) file 
-
     songs = read_data(filename)
 
-    # TODO: 
-    # - Similar artist column contains a string of artist ids separated by "|"
-    # -- Should we display artist names instead?
 
 if __name__ == '__main__':
     main(sys.argv[1])
