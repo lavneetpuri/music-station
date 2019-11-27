14e7c6f0be45f2f487d10b763c7cdbf13d7eb3c3
Lavneet Puri <lavneetp@sfu.ca>
2019-11-27 15:38:30 +0000

    i11-use combined songs.csv when loading songs from data module

--- data/main.py
+++ data/main.py
@@ -2,4 +2,4 @@ import pandas as pd
 import os
 
 dir_path = os.path.dirname(__file__)
-songs = pd.read_csv(os.path.join(dir_path, '10000_Songs_A_H5.csv'))
+songs = pd.read_csv(os.path.join(dir_path, 'songs.csv'))
