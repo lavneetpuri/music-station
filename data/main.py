524279243f381208822d82b835e44921b4d5930d
Lavneet Puri <lavneetp@sfu.ca>
2019-12-01 12:40:21 +0000

    i3-update data module to return dataframes index by song/artist id

--- data/main.py
+++ data/main.py
@@ -3,5 +3,9 @@ import os
 
 dir_path = os.path.dirname(__file__)
 
-songs = pd.read_csv(os.path.join(dir_path, 'songs.csv'))
-artists = pd.read_csv(os.path.join(dir_path, 'artists.csv'))
+songs = pd.read_csv(os.path.join(dir_path, 'songs.csv'), index_col='track_id')
+artists = pd.read_csv(
+            os.path.join(dir_path, 'artists.csv'), index_col='artist_id'
+          )
+# drop row count column
+artists = artists.drop(artists.columns[0], 1)
