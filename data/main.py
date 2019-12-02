2421515c8b48290997c37acda949c76acf182b83
Lavneet Puri <lavneetp@sfu.ca>
2019-12-02 02:51:56 +0000

    i15-add todo about filtering invalid rows

--- data/main.py
+++ data/main.py
@@ -4,6 +4,11 @@ import os
 dir_path = os.path.dirname(__file__)
 
 songs = pd.read_csv(os.path.join(dir_path, 'songs.csv'), index_col='track_id')
+
+# TODO: filter invalid rows
+# we are losing too much data
+# songs = songs[songs.year != 0]
+
 artists = pd.read_csv(
             os.path.join(dir_path, 'artists.csv'), index_col='artist_id'
           )
